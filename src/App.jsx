4df226b415af86c254b8d8d8b414074b1d144ef0
hom619 <hom.grg619@gmail.com>
2024-11-26 14:45:33 +1100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      Hello Movie World
+      <button className="btn btn-primary">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
