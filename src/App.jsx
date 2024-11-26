23882212b304acf77970464ff617a07a3a37c450
hom619 <hom.grg619@gmail.com>
2024-11-27 00:27:26 +1100

    Nav bar and hero section UI created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
-
+import { Hero } from "./components/Hero";
 function App() {
   return (
-    <div>
-      Hello Movie World
-      <button className="btn btn-primary">Click Me</button>
+    <div className="wrapper">
+      {/* Hero section */}
+      <Hero />
+      {/* Display section */}
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+export const Hero = () => {
+  const movieCSS = {
+    backgroundImage: `url("https://www.omdbapi.com/src/poster.jpg")`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    height: "60vh",
+  };
+  return (
+    <div>
+      <nav className="py-3 text-danger fixed-top">
+        <h2 className="container">The world of Movies</h2>
+      </nav>
+      <div
+        className="hero d-flex justify-content-center align-items-center text-light"
+        style={movieCSS}
+      >
+        <div className="hero-content">
+          <div className="form-center">
+            <div className="text-center">
+              <h1>This is the world of Movies</h1>
+              <p>Find the details before watching them</p>
+            </div>
+          </div>
+          <div className="input-group my-5">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search Movie Name"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+            />
+            <div className="input-group-append">
+              <button className="btn btn-danger" type="button">
+                Button
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
